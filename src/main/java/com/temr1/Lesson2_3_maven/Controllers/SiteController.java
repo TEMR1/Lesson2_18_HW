68c31b999bbf0bf4a531b70d7d1be92579aa4107
Danylo <danylo.shpak.2009@gmail.com>
2024-01-25 19:03:44 +0200

    Create new page

--- src/main/java/com/temr1/Lesson2_3_maven/Controllers/SiteController.java
+++ src/main/java/com/temr1/Lesson2_3_maven/Controllers/SiteController.java
@@ -1,12 +1,18 @@
 package com.temr1.Lesson2_3_maven.Controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+public class SiteController {
     @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "<a href='/mySite'>My Site</a>";
+    }
+
+    @GetMapping("/mySite")
+    public String mySite() {
+        return "Hello";
     }
 }
