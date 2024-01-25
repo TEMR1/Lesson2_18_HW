5079dc30fd7e863827981024c1abc98b956a0008
Danylo <danylo.shpak.2009@gmail.com>
2024-01-25 18:05:31 +0200

    Project working

---
+++ src/main/java/com/temr1/Lesson2_3_maven/Main.java
@@ -0,0 +1,11 @@
+package com.temr1.Lesson2_3_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
