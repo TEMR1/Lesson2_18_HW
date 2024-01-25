c503447e647b57162b40a36ec1dc423b4c5ad72e
Danylo <danylo.shpak.2009@gmail.com>
2024-01-25 18:16:08 +0200

    Create packages

--- src/main/java/com/temr1/Lesson2_3_maven/Controllers/Controller.java
+++ src/main/java/com/temr1/Lesson2_3_maven/Controllers/Controller.java
@@ -1,4 +1,5 @@
-package com.temr1.Lesson2_3_maven;
+package com.temr1.Lesson2_3_maven.Controllers;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
